4bd30a7f41b1be90be5b602ff44e0ebc951838fb
limdabeen <ldb74777@naver.com>
2025-04-11 02:11:57 +0900

    회원 도메인 및 서비스 로직 + 테스트 코드 구현

---
+++ hello-spring/src/test/java/hello/hello_spring/service/MemberServiceTest.java
@@ -0,0 +1,76 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    void setUp() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    void tearDown() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("spring");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member result = memberRepository.findById(saveId).get();
+        assertThat(result.getName()).isEqualTo("spring");
+    }
+
+    @Test
+    void 전체회원조회() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring1");
+        memberService.join(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        memberService.join(member2);
+
+        // when
+        List<Member> result = memberService.findMembers();
+
+        // then
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+    @Test
+    void 회원아이디조회() {
+        // given
+        Member member = new Member();
+        member.setName("spring");
+        Long savedId = memberService.join(member);
+
+        // when
+        Optional<Member> result = memberService.findOne(savedId);
+
+        // then
+        assertThat(result).isPresent();
+        assertThat(result.get().getName()).isEqualTo("spring");
+    }
+}
